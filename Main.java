e0d6acae0efa54c402c67521e7f4aef7f91cd63e
Yusef <Yusef@gmail.sef>
2022-09-30 09:47:57 +0200

    suuuuuuu

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JApplet;
+import java.awt.Graphics;
+
+public class Hello extends JApplet {
+
+    public void paint(Graphics g) {
+        g.drawString("Hola, Yusef!", 65, 95);
+    }
+
+    public.static.void("yusef");
+
+}
